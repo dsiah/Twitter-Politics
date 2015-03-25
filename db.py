6849f8642673bd2229151ce891f59901639fa6f4
David Siah <dsiah@villanova.edu>
2015-03-24 23:56:03 -0400

    changed db connection script to allow for CLI ip entry

--- db.py
+++ db.py
@@ -1,18 +1,26 @@
 from pymongo import MongoClient
+import argparse
 
+parser = argparse.ArgumentParser(description='Set the IP address')
+parser.add_argument('ip_address', help='Mandatory: IP Address of mongodb server', 
+					action='store')
+	
 # change localhost to the IP of the machine
-client = MongoClient('153.104.156.100', 27017)
-
-# test db is tweets1, collection is testData
-db = client.tweets1
-collection = db.testData
-
-#more comments
-listo = collection.find()
-
 class PoliticsDB:
 
-	def __init__(self, ipaddress):
+	def __init__ (self, ipaddress):
 		self.ipaddress = ipaddress
+		self.client = MongoClient(ipaddress, 27017)
+		self.db = self.client.tweets1
+		self.collection = self.db.testData
 
-pdb = PoliticsDB(129391923)
+	def getInfo (self):
+		print self.db
+		print self.collection
+		print self.ipaddress
+
+
+if __name__ == '__main__':
+	args = parser.parse_args()	
+	#print args.ip_address
+	pdb = PoliticsDB(args.ip_address)
