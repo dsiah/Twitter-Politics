ff6272609142423201d89daf6585e42fd653dcd5
kentkwu <kwu@villanova.edu>
2015-04-08 20:15:38 -0400

    Updated for timestamp

--- tweets_to_db.py
+++ tweets_to_db.py
@@ -3,27 +3,30 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from pymongo import MongoClient
-from politics_keywords_refined import politics_keywords
+from politics_keywords import politics_keywords
 
 import json
 import credentials # custom security file MUST-HAVE
-
-#This script prints the output to the command line, not to the database
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
         tweet = json.loads(data.encode('ascii', 'ignore')) # encode to ascii to use decode json
-        if 'text' in tweet:
-            #print tweet['text'], tweet['timestamp_ms']
-            print tweet['text'], tweet['timestamp_ms']
+        print "Wrote tweet" , tweet['id']
+        db = client.tweets1
+        collection = db.day1
+
+        collection.insert({ 'tweetId': tweet['id'], 'text': tweet['text'],
+                            'timestamp_ms': tweet['timestamp_ms'] })
         return True
 
     def on_error(self, status):
         print status
 
 if __name__ == '__main__':
+    client = MongoClient('localhost', 27017)
+
     #This handles Twitter authentification and the connection to Twitter Streaming API
     l = StdOutListener()
     auth = OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
