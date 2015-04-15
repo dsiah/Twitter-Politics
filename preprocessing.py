2abf3c6b11b39ad138f69ae722cb4eafad9111d8
kentkwu <kwu@villanova.edu>
2015-04-14 22:14:34 -0400

    Write tweets to topics

--- preprocessing.py
+++ preprocessing.py
@@ -31,9 +31,8 @@ def count_vectorize(tweets):
     stopwords = stopwords + [ '.', ',', ':', ';', 's', '#', '@', '&', 'rt', "''", '``', 
                               '?', '!', '$', '...', "'s", "n't", "'", "--", '-', '%', '|',
                               '/', '(', ')' ]
-
-    vectorizer = CountVectorizer(min_df=1, tokenizer=tokenize_for_vectorize, 
-                                 stop_words = stopwords, ngram_range=(1,2))
+    vectorizer = CountVectorizer(min_df=1, tokenizer=tokenize, 
+                                 stop_words = stopwords) #ngram_range=(1,2))
     count_vector = vectorizer.fit_transform(tweets)
     print "count_vector shape: " + str(count_vector.shape)
     return (count_vector, vectorizer.get_feature_names())
@@ -44,22 +43,25 @@ def get_topics_lda(tfidf_matrix_and_vocab, tweets):
     model = lda.LDA(n_topics=30, n_iter=500, random_state=1)
     model.fit(tfidf_matrix)
     topic_word = model.topic_word_
-    n_top_words = 8
-    for i, topic_dist in enumerate(topic_word):
-        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
-        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+    #n_top_words = 8
+    #for i, topic_dist in enumerate(topic_word):
+        #topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+        #print('Topic {}: {}'.format(i, ' '.join(topic_words)))
     doc_topic = model.doc_topic_
-    for i in range(30):
-        print("{} (top topic: {})".format(tweets[i], doc_topic[i].argmax()))
-    print doc_topic[0]
+    for i in range(len(tweets)):
+        with open ('./topics/topic{}.txt'.format(doc_topic[i].argmax()), 'a+') as dest:
+            dest.write(tweets[i] + "\n")
+            print "writing tweet {0} to topic {1}".format(i, doc_topic[i].argmax())
+        #print("{} (top topic: {})".format(tweets[i], doc_topic[i].argmax()))
+    #print doc_topic[0]
     return True
+
 
 if __name__=="__main__":
     #remove_links('politician_text_test.txt', 'politician_text_processed.txt')
     tweets = load_tweets('politician_text_processed.txt')
-    print tokenize_tweets_unigrams(tweets)
     #print tokenize_tweets_unigrams(tweets)
-    print count_and_tf_idf(tweets)[1]
+    #print tokenize_tweets_unigrams(tweets)
     print get_topics_lda(count_vectorize(tweets), tweets)
     #print get_topics_lda(tf_idf(count_vectorize(tweets)), tweets)
 
